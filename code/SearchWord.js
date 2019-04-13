1fbd455e7034a00ce9c1782c56a49907becbd1ee
Yuliia Deineha <ydeineha@e1z4r14p9.42.us.org>
2019-04-12 20:24:26 -0700

    basic dictionary functionality & word search

---
+++ code/SearchWord.js
@@ -0,0 +1,16 @@
+var http = require('http')
+var console = require('console')
+var config = require('config')
+
+module.exports.function = function searchWord (word) {
+  console.log("SearchWord in a dictionary")
+  var options = { 
+    format: 'json',
+     query: {
+      search: word
+    }
+  };
+  // If word is "apple", then this makes a GET call to /search?search=apple
+  var response = http.getUrl(config.get('remote.url') + '/search', options);
+  return response;
+}
